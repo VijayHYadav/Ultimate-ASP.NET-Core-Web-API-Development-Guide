d155c8136e77a2a662cf4a646d41f029f879a69f
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-19 22:31:01 +0530

    004. Refactor GET Methods

---
+++ HotelListing.API/Models/Country/GetCountryDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using HotelListing.API.Models.Hotel;
+
+namespace HotelListing.API.Models.Country
+{
+    public class GetCountryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+    }
+
+}
+
+
+// Rule of thumb: A DTO shouldn't have field that is directly relate a model type or data model
