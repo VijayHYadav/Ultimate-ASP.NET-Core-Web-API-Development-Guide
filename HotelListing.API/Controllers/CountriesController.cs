fec958197753a9b820f4c025f37d503ec049d07b
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-19 20:15:46 +0530

    003. Introducing AutoMapper

--- HotelListing.API/Controllers/CountriesController.cs
+++ HotelListing.API/Controllers/CountriesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using HotelListing.API.Data;
 using HotelListing.API.Models.Country;
+using AutoMapper;
 
 namespace HotelListing.API.Controllers
 {
@@ -15,10 +16,12 @@ namespace HotelListing.API.Controllers
     public class CountriesController : ControllerBase
     {
         private readonly HotelListingDbContext _context;
+        private readonly IMapper _mapper;
 
-        public CountriesController(HotelListingDbContext context)
+        public CountriesController(HotelListingDbContext context, IMapper mapper)
         {
             _context = context;
+            this._mapper = mapper;
         }
 
         // GET: api/Countries
@@ -79,11 +82,13 @@ namespace HotelListing.API.Controllers
         [HttpPost]
         public async Task<ActionResult<Country>> PostCountry(CreateCountryDto createCountry)
         {
-            var country = new Country
-            {
-                Name = createCountry.Name,
-                ShortName = createCountry.ShortName,
-            };
+            //var countryOld = new Country
+            //{
+            //    Name = createCountry.Name,
+            //    ShortName = createCountry.ShortName,
+            //};
+
+            var country = _mapper.Map<Country>(createCountry);
 
             _context.Countries.Add(country);
             await _context.SaveChangesAsync();
